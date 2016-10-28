a1b9f60c0c6d40693fed17784502a278b07de86e
Aaron D Borden <adborden@a14n.net>
2016-10-28 09:17:45 -0700

    Remove unused methods

--- static_src/components/navbar.jsx
+++ static_src/components/navbar.jsx
@@ -3,8 +3,6 @@ import style from 'cloudgov-style/css/cloudgov-style.css';
 import React from 'react';
 
 import createStyler from '../util/create_styler';
-import spaceActions from '../actions/space_actions.js';
-import orgActions from '../actions/org_actions.js';
 import AppStore from '../stores/app_store.js';
 import OrgStore from '../stores/org_store.js';
 import SpaceStore from '../stores/space_store.js';
@@ -30,7 +28,6 @@ export class Nav extends React.Component {
     this.state = stateSetter();
     this.styler = createStyler(style);
     this._onChange = this._onChange.bind(this);
-    this._handleOverviewClick = this._handleOverviewClick.bind(this);
     this._handleOrgSelect = this._handleOrgSelect.bind(this);
     this._handleSpaceSelect = this._handleSpaceSelect.bind(this);
     this._handleAppSelect = this._handleAppSelect.bind(this);
@@ -50,20 +47,6 @@ export class Nav extends React.Component {
     this.setState(stateSetter());
   }
 
-  _handleOverviewClick() {
-    orgActions.toggleSpaceMenu('0');
-    spaceActions.changeCurrentSpace('0');
-  }
-
-  _handleOrgClick(orgGuid) {
-    orgActions.toggleSpaceMenu(orgGuid);
-  }
-
-  _toggleSpacesMenu(orgGuid, ev) {
-    ev.preventDefault();
-    orgActions.toggleSpaceMenu(orgGuid);
-  }
-
   _handleOrgSelect(e) {
     const orgGuid = e.target.value;
     router.setRoute(`/org/${orgGuid}`);
@@ -80,38 +63,6 @@ export class Nav extends React.Component {
     const spaceGuid = this.state.currentSpace.guid;
     const appGuid = e.target.value;
     router.setRoute(`/org/${orgGuid}/spaces/${spaceGuid}/apps/${appGuid}`);
-  }
-
-  // currently displays the space listing
-  orgHref(org) {
-    return `/#/org/${org.guid}`;
-  }
-
-  orgSubHref(org, linkHref) {
-    return this.orgHref(org) + linkHref;
-  }
-
-  marketplaceHref(org) {
-    return this.orgSubHref(org, '/marketplace');
-  }
-
-  spaceHref(org, spaceGuid) {
-    return this.orgSubHref(org, `/spaces/${spaceGuid}`);
-  }
-
-  isCurrentApp(appGuid) {
-    const currentApp = this.state.currentApp;
-    return currentApp && currentApp.guid === appGuid;
-  }
-
-  isCurrentOrg(orgGuid) {
-    const currentOrg = this.state.currentOrg;
-    return currentOrg && currentOrg.guid === orgGuid;
-  }
-
-  isCurrentSpace(spaceGuid) {
-    const currentSpace = this.state.currentSpace;
-    return currentSpace && currentSpace.guid === spaceGuid;
   }
 
   render() {
@@ -173,9 +124,3 @@ export class Nav extends React.Component {
     );
   }
 }
-Nav.propTypes = {
-  subLinks: React.PropTypes.array
-};
-Nav.defaultProps = {
-  subLinks: []
-};
